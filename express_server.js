860587609aec48943e7bc44f5a4f82864259ab67
v-lane <victoria.lane.m@gmail.com>
2024-02-20 15:58:13 -0500

    Add column with button to delete URLs on /urls

--- express_server.js
+++ express_server.js
@@ -3,6 +3,8 @@ const app = express();
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -23,12 +25,15 @@ function generateRandomString() {
 
 
 
-app.use(express.urlencoded({ extended: true }));
-
 app.post("/urls", (req, res) => {
   const id = generateRandomString();
-  urlDatabase[id] = req.body.longURL
-  res.redirect(`/urls/${id}`)
+  urlDatabase[id] = req.body.longURL;
+  res.redirect(`/urls/${id}`);
+});
+
+app.post("/urls/:id/delete", (req, res) => {
+  delete urlDatabase[req.params.id];
+  res.redirect('/urls');
 });
 
 app.get("/", (req, res) => {
@@ -38,7 +43,7 @@ app.get("/", (req, res) => {
 app.get('/u/:id', (req, res) => {
   const longURL = urlDatabase[req.params.id];
   res.redirect(longURL);
-})
+});
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
