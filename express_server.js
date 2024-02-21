00897bf07211764ffb97c920f04d7017311bdc0a
v-lane <victoria.lane.m@gmail.com>
2024-02-21 11:34:17 -0500

    Added logout client & server logic

--- express_server.js
+++ express_server.js
@@ -31,6 +31,11 @@ app.post("/login", (req, res) => {
   res.redirect('/urls');
 });
 
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
+  res.redirect("/urls")
+});
+
 app.post("/urls", (req, res) => {
   const id = generateRandomString();
   urlDatabase[id] = req.body.longURL;
